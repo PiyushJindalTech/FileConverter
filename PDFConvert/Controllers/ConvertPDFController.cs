ab44f6db3428fbfb4e97863169bddc5ac4211874
Piyush <piyushjindal@hirecraft.in>
2020-08-17 18:26:27 +0530

    Initial-Commit

---
+++ PDFConvert/Controllers/ConvertPDFController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Imagine_Converter;
+using System.IO;
+
+
+namespace PDFConvert.Controllers
+{
+    public class ConvertPDFController : Controller
+    {
+        beFileConverter fileConverter = new beFileConverter();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index(string id)
+        {
+            TempData["id"] = id;
+            TempData.Keep();
+            return View();
+
+        }
+
+        [HttpPost]
+        public void ConvertFile(HttpPostedFileBase flpExcel)
+        {
+            try
+            {
+                string id = "";
+                if (TempData["id"] != null)
+                    id = Convert.ToString(TempData["id"]);
+
+                string filePath = Server.MapPath("~/ExportFiles/");
+                flpExcel.SaveAs(filePath + flpExcel.FileName);
+                string outputFileName = "";
+                string fileExtension = Path.GetExtension(flpExcel.FileName).ToLower().Replace(".", "");
+                string tStatus = "";
+
+                if (fileExtension == "doc" || fileExtension == "docx")
+                    tStatus = fileConverter.ExportWordToPdf(filePath, flpExcel.FileName, out outputFileName);
+
+                else if (fileExtension == "xls" || fileExtension == "xlsx" || fileExtension == "ods" || fileExtension == "csv")
+                    tStatus = fileConverter.ExportExcelToPdf(filePath, flpExcel.FileName, out outputFileName);
+
+                else if (fileExtension == "ppt" || fileExtension == "pptx")
+                    tStatus = fileConverter.ExportPowerPointToPdf(filePath, flpExcel.FileName, out outputFileName);
+
+                else if (fileExtension == "jpg" || fileExtension == "jpeg" || fileExtension == "png" || fileExtension == "svg" || fileExtension == "gif")
+                    tStatus = fileConverter.ExportToImageToPdf(filePath, flpExcel.FileName, out outputFileName);
+
+                else if (fileExtension == "pdf")
+                {
+                    if (id == "PDFtoWord")
+                        tStatus = fileConverter.ExportPdfToWord(filePath, flpExcel.FileName, out outputFileName);
+
+                    else if (id == "PDFtoExcel")
+                        tStatus = fileConverter.ExportPdfToExcel(filePath, flpExcel.FileName, out outputFileName);
+
+                    //else if (id == "PDFtoPPT")
+                    //    tStatus = fileConverter.ExportPdfToPowerPoint(filePath, flpExcel.FileName, out outputFileName);
+
+                    else
+                        tStatus = "";
+                }
+
+                else
+                    tStatus = "";
+
+                if (tStatus != "1")
+                    Helper.ErrorLog("ConvertFile(): " + tStatus);
+                else
+                    DownloadFile(filePath, outputFileName, Helper.GetContentType(outputFileName.Split('.').Last()));
+            }
+            catch (Exception ex)
+            {
+                Helper.ErrorLog(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+
+        public void DownloadFile(string sourcePath, string fileName, string contentType)
+        {
+            try
+            {
+                Response.ContentType = contentType;
+                Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+                Response.TransmitFile(sourcePath + fileName);
+                Response.Flush();
+                Response.SuppressContent = true;
+                //HttpContext.Current.ApplicationInstance.CompleteRequest();
+                Helper.DeleteFile(sourcePath + fileName);
+            }
+            catch (Exception ex)
+            {
+                Helper.ErrorLog(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+        }
+
+
+    }
+}
---
+++ PDFConvert/Helper.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace PDFConvert
+{
+    public class Helper
+    {
+        public static string GetContentType(string fileExtension)
+        {
+            string contentType = "";
+            switch (fileExtension.ToLower())
+            {
+                case "doc":
+                    contentType = "application/msword";
+                    break;
+                case "docx":
+                    contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                    break;
+                case "xls":
+                    contentType = "application/vnd.ms-excel";
+                    break;
+                case "xlsx":
+                    contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    break;
+                case "pdf":
+                    contentType = "application/pdf";
+                    break;
+                case "ppt":
+                    contentType = "application/vnd.ms-powerpoint";
+                    break;
+                case "pptx":
+                    contentType = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                    break;
+                case "jpg":
+                    contentType = "image/jpeg";
+                    break;
+                case "jpeg":
+                    contentType = "image/jpeg";
+                    break;
+                case "png":
+                    contentType = "image/png";
+                    break;
+                case "svg":
+                    contentType = "image/svg+xml";
+                    break;
+                case "bmp":
+                    contentType = "image/bmp";
+                    break;
+                case "gif":
+                    contentType = "image/gif";
+                    break;
+                case "ico":
+                    contentType = "image/vnd.microsoft.icon";
+                    break;
+                case "tif":
+                    contentType = "image/tiff";
+                    break;
+                case "tiff":
+                    contentType = "image/tiff";
+                    break;
+                case "webp":
+                    contentType = "image/webp";
+                    break;
+                case "txt":
+                    contentType = "text/plain";
+                    break;
+                default:
+                    contentType = "text/plain";
+                    break;
+            }
+            return contentType;
+        }
+
+        public static void ErrorLog(string errMsg)
+        {
+
+            string LogPath = System.Configuration.ConfigurationManager.AppSettings["LogPath"];
+            if (!Directory.Exists(LogPath + @"\Log"))
+                Directory.CreateDirectory(LogPath + @"\Log");
+
+            string repeatChar = new string('*', 50);
+
+            File.AppendAllText(LogPath + @"\Log\Log_" + DateTime.Now.ToString("dd_MMM_yyyy") + ".txt", repeatChar + Environment.NewLine + "Error Date and time: " + DateTime.Now.ToString() + Environment.NewLine + errMsg + Environment.NewLine + repeatChar + Environment.NewLine);
+
+        }
+
+        public static void DeleteFile(string _path)
+        {
+            if (File.Exists(_path))
+                File.Delete(_path);
+        }
+
+        public static void DisplaySweetAlertPopup(Page pg, string title, string message, MessageType messagetype)
+        {
+            try
+            {
+                string tScript = "showPopup('" + title + "', '" + message + "', '" + messagetype + "')";
+                ScriptManager.RegisterStartupScript(pg, pg.GetType(), "msg", tScript, true);
+            }
+            catch (Exception) { }
+        }
+
+        public static void DisplayPopWithoutConfirmation(Page pg, string title, string message, MessageType messagetype)
+        {
+            try
+            {
+                string tScript = "DisplayPopWithoutConfirmation('" + title + "', '" + message + "', '" + messagetype + "')";
+                ScriptManager.RegisterStartupScript(pg, pg.GetType(), "msg", tScript, true);
+            }
+            catch (Exception) { }
+        }
+    }
+    public enum MessageType
+    {
+        success = 1,
+        error = 2,
+        info = 3,
+    }
+}
